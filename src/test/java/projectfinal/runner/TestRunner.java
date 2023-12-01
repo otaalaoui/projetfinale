7439bb39b53c5ec91d69f6a284d3603deaa8d515
Oussama Taalaoui <otaalaoui@dom.tti>
2023-12-01 16:08:09 +0100

    projet

--- src/test/java/projectfinal/runner/TestRunner.java
+++ src/test/java/projectfinal/runner/TestRunner.java
@@ -7,8 +7,9 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources/Features", glue = {
-		"projectfinal.stepsdef" }, monochrome = true, plugin = { "pretty", "html:target/cucumber-reports.html",
-				"json:target/cucumber.json", "junit:target/cucumber-results.xml" }, tags = "")
+		"projectfinal.stepsdef" }, monochrome = true, plugin = {
+				"pretty", "html:target/cucumber-reports.html", "json:target/cucumber.json","json:target/rapport.json",
+				"junit:target/cucumber-results.xml" }, tags = "")
 public class TestRunner {
 
 }
